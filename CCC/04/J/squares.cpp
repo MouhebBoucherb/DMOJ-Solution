dfcb1cbde0c6d0794cc8488b1907c1342513bb24
MouhebBoucherb <mouhebrock55@gmail.com>
2017-02-06 17:43:51 +0100

    Create squares.cpp

---
+++ CCC/04/J/squares.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    double n;
+    cin>>n;
+    cout<<"The largest square has side length "<<floor(sqrt(n))<<".\n";
+    return 0;
+}
